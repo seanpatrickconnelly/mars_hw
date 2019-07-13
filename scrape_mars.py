3cfb33cb8272178bafd4a2c9d4a4d1145bc525ac
seanpatrickconnelly <seanpconnelly@hotmail.com>
2019-07-13 12:45:12 -0500

    submitting mars homework
    Thanks again to all the TAs for all you do but esp for wading through (my) homework!

---
+++ scrape_mars.py
@@ -0,0 +1,57 @@
+# Start by converting your Jupyter notebook into a Python script called 
+# scrape_mars.py with a function called scrape that will execute all of 
+# your scraping code from above and return one Python dictionary containing 
+# all of the scraped data. Next, create a route called /scrape that will import 
+# your scrape_mars.py script and call your scrape function.
+# Store the return value in Mongo as a Python dictionary.
+
+
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape_info():
+    browser = init_browser()
+
+    
+    url = "https://visitcostarica.herokuapp.com/"
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    # Get the average temps
+    avg_temps = soup.find('div', id='weather')
+
+    # Get the min avg temp
+    min_temp = avg_temps.find_all('strong')[0].text
+
+    # Get the max avg temp
+    max_temp = avg_temps.find_all('strong')[1].text
+
+    # BONUS: Find the src for the sloth image
+    relative_image_path = soup.find_all('img')[2]["src"]
+    sloth_img = url + relative_image_path
+
+    # Store data in a dictionary
+    costa_data = {
+        "sloth_img": sloth_img,
+        "min_temp": min_temp,
+        "max_temp": max_temp
+    }
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # Return results
+    return costa_data
